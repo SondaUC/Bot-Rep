a0697a4b37499727b71e33dd6e932877ea04be8c
Martin Concha <martincb@uc.cl>
2017-12-06 20:22:35 -0300

    Bot

--- Bot/main.py
+++ Bot/main.py
@@ -7,4 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-    return """Holi"""
+    return """Ganzos y patos han tenido guerras interminables las cuales
+solo han tenido como resultado el caos y la destruccion.
+
+La comida escasea, y las familias de ambos lados sufren, amor y traicion existe entre los ganzos y patos,
+y solo el poder de los asados determinara quien de los 2 partidos sera el vencedor
+(Es porque el que cacha como hacer asados se puede hacer el tremendo festin con el otro)
+(Pd: equis de xD)"""
+
+
+if __name__ == '__main__':
+    app.run()
