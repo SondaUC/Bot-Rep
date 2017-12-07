ff3aae4fd50984ae18b905bfae809933875c08d8
Martin Concha <martincb@uc.cl>
2017-12-06 23:19:41 -0300

    Bot

--- Bot/main.py
+++ Bot/main.py
@@ -1,3 +1,5 @@
+#git subtree push --prefix Bot heroku master
+
 from flask import Flask, request
 import requests
 import json
@@ -7,13 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-    return """Ganzos y patos han tenido guerras interminables las cuales
-solo han tenido como resultado el caos y la destruccion.
-
-La comida escasea, y las familias de ambos lados sufren, amor y traicion existe entre los ganzos y patos,
-y solo el poder de los asados determinara quien de los 2 partidos sera el vencedor
-(Es porque el que cacha como hacer asados se puede hacer el tremendo festin con el otro)
-(Pd: equis de xD)"""
+    return """Funciona :D"""
 
 
 if __name__ == '__main__':
