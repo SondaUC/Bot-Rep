7cf5294abbb67f68843c3083b9be2e018de9c138
Martin Concha <martincb@uc.cl>
2017-12-07 12:27:11 -0300

    Bot

--- Bot/main.py
+++ Bot/main.py
@@ -27,6 +27,19 @@ def telegram():
         text = "El programa recibio que tiene un problema"
         print("cambia el texto")
 
+        if b["message"]["text"][0:5] == "Start":
+            text = """Hola!
+
+Soy SondaBot, en que te puedo ayudar?"""
+            print("cambia el texto")
+
+        if b["message"]["text"][0:48]  == "Tengo un error en mi proceso de calculo de cuota":
+            text = "Su problema se ha registrado, para completar el proceso ingrese su usuario"
+
+        if b["message"]["text"][0:21] == "Paola Cabezas Pizarro":
+            text = """Posiblemente, su problema sea de un registro duplicado. De todas maneras,
+la orden de servicio ya se ha presentado a nuestro equipo, y estamos trabajando en ella ahora mismo"""
+
     form = {'chat_id': b["message"]["chat"]["id"], 'text': text}
     header = {'content-Type': 'application/json'}  # el header define el tipo de contenido
     r = requests.post('https://api.telegram.org/bot495424347:AAGM7XlCXne39odbh-aS95MyuBlkFpWBkuI/sendMessage',
