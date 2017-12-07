182431cc528d51aaa6e04030b4b6c7c6e6af5325
Martin Concha <martincb@uc.cl>
2017-12-07 12:32:10 -0300

    Bot

--- Bot/main.py
+++ Bot/main.py
@@ -34,11 +34,10 @@ Soy SondaBot, en que te puedo ayudar?"""
         print("cambia el texto")
 
     if b["message"]["text"][0:48]  == "Tengo un error en mi proceso de calculo de cuota":
-        text = "Su problema se ha registrado, para completar el proceso ingrese su usuario"
+        text = "Su problema se ha registrado, para completar el proceso ingrese su usuario:"
 
     if b["message"]["text"][0:21] == "Paola Cabezas Pizarro":
-        text = """Posiblemente, su problema sea de un registro duplicado. De todas maneras,
-la orden de servicio ya se ha presentado a nuestro equipo, y estamos trabajando en ella ahora mismo"""
+        text = """Posiblemente, su problema sea de un registro duplicado. De todas maneras, la orden de servicio ya se ha presentado a nuestro equipo, y estamos trabajando en ella ahora mismo"""
 
     form = {'chat_id': b["message"]["chat"]["id"], 'text': text}
     header = {'content-Type': 'application/json'}  # el header define el tipo de contenido
