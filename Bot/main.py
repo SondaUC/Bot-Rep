8ec3f39cec7919bc1cb264c5c1bd65a7eeef1fb5
Martin Concha <martincb@uc.cl>
2017-12-07 00:17:53 -0300

    Bot

--- Bot/main.py
+++ Bot/main.py
@@ -12,5 +12,23 @@ def hello():
     return """Funciona :D"""
 
 
+@app.route('/telegram', methods=['POST'])
+def telegram():
+    a = request.data
+    b = json.loads(a)
+    mensaje = b["message"]["text"]
+    text = ""
+
+    if b["message"]["text"][0:4] == "Tengo un problema":
+        text = "El programa recibio que tiene un problema"
+
+    form = {'chat_id': b["message"]["chat"]["id"], 'text': text}
+    header = {'content-Type': 'application/json'}  # el header define el tipo de contenido
+    r = requests.post('https://api.telegram.org/bot402191911:AAFH1X4HxsZny4eVvp3_vLxiSzQe2McsIdU/sendMessage',
+                      headers=header, data=json.dumps(form))
+
+    return "Cualquier Cosa"
+
+
 if __name__ == '__main__':
     app.run()
